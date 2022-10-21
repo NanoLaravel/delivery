73284d2b89f932c46eec7e7010e153fede3a1a9e
Hernando Ortega <51216975+NanoLaravel@users.noreply.github.com>
2022-10-21 07:35:54 -0500

    termine activity registro con el respectivo intent para llegar aqui

--- app/src/main/java/com/herfan/delivery/MainActivity.java
+++ app/src/main/java/com/herfan/delivery/MainActivity.java
@@ -2,13 +2,27 @@ package com.herfan.delivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnregistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnregistro = findViewById(R.id.btnregistro);
+        btnregistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Registro.class);
+                startActivity(i);
+            }
+        });
+
+
     }
+
 }
